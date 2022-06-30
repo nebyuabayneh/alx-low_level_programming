e65314e54caad6d30daa3d82bd5b7f99012d31a3
Nebyu <nebyouabayneh@gmail.com>
2022-06-30 17:31:28 +0300

    C programming

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,11 +2,10 @@
 /**
  * main - void
  * 
- * Description: 'the program's displace a string without the fuction called prin'
+ * Description: 'the program's displace a string without the fuction called print'
  * 
  * Return: Always 0 (Success)
  */
-
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
