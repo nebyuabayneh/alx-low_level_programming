5e18bc0954c825439c4182dbee16c5e75fffe682
Nebyu <nebyouabayneh@gmail.com>
2022-07-05 19:51:51 +0300

    Time table up to 9

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,17 @@
+#include <unistd.h>
+#include "main.h"
+
+/**
+ * times_table - Check the code
+ * Description: return 1 if c is lowercase otherwise return 0
+ * Return: Nothing.
+ */
+void times_table(void)
+{
+    int N;
+    for (i = 0; i < N; i++)
+    {
+        /* code */
+    }
+
+}
