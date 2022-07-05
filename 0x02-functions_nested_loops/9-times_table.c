8228c65c2a08b85c500bcb8fea2a9c96b41309bd
Nebyu <nebyouabayneh@gmail.com>
2022-07-05 20:58:36 +0300

    Time table up to 9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -3,15 +3,40 @@
 
 /**
  * times_table - Check the code
- * Description: return 1 if c is lowercase otherwise return 0
+ * Description: Time table up to 9
  * Return: Nothing.
  */
 void times_table(void)
 {
-    int N;
-    for (i = 0; i < N; i++)
-    {
-        /* code */
-    }
+	int i, j, n;
 
+	for (i = 0; i <= 9; i++)
+	{
+		for (j = 0; j <= 9; j++)
+		{
+			n = i * j;
+
+			if ((n / 10) == 0)
+			{
+				if (j != 0)
+					_putchar(' ');
+				_putchar(n + '0');
+
+				if (j == 9)
+					continue;
+				_putchar(',');
+				_putchar(' ');
+			}
+			else
+			{
+				_putchar((n / 10) + '0');
+				_putchar((n % 10) + '0');
+				if (j == 9)
+					continue;
+				_putchar(',');
+				_putchar(' ');
+			}
+		}
+		_putchar('\n');
+	}
 }
