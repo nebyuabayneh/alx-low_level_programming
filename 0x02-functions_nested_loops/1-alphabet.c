ef37a28bc1d07222985a263c1a8aa568ed7fb16f
Nebyu <nebyouabayneh@gmail.com>
2022-07-05 16:04:39 +0300

    alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include <unistd.h>
+#include "main.h"
+
+/**
+ * main - Print _putchar using the function called main
+ * Description: It prints the word Holberton, followed by a new line.
+ * Return: 0.
+ */
+int main(void)
+{
+	//char word[] = "_putchar";
+	int i;
+
+	for (i = 'a'; i <= 'z'; i++)
+		_putchar(word[i]);
+	_putchar('\n');
+
+	return (0);
+}
