5cda0eebff0221a97496b21911421afda672c73b
Nebyu <nebyouabayneh@gmail.com>
2022-07-05 21:49:32 +0300

    Return the sum of two numbers

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -3,13 +3,10 @@
 
 /**
  * int add - Check the code
- * Description: return the sum of two numbers
+ * Description: Return the sum of two numbers
  * Return: Always 0.
  */
 int add(int a, int b)
 {
-    int sum;
-    sum = add(a,b);
-    printf("%d\n", sum);
-    return(0);
+	return (a + b);
 }
