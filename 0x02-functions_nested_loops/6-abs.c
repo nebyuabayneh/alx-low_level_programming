8b9994ec9cb5a45eee366d17454f4bf72c7ba3e8
Nebyu <nebyouabayneh@gmail.com>
2022-07-05 19:46:49 +0300

    Returns the absolute value of a number

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * int _abs - Check the code
+ * Description: return 1 if c is lowercase otherwise return 0
+ * Return: The absolute value of a number.
+ */
+int _abs(int r)
+{
+    if (r >= 0)
+		return (r);
+	else
+		return (r * -1);
+}
