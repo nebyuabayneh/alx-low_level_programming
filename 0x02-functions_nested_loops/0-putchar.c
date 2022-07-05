222bcebd8b280aabd12ee3ad76d45cf926375bd6
Nebyu <nebyouabayneh@gmail.com>
2022-07-05 15:24:33 +0300

    putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,1 +1,21 @@
+#include <main.h>
 
+**
+ * main - print _putchar using the function called main
+ * @c: The character to print
+ *
+ * Return: On success 0.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int main(void)
+{
+    _putchar("_")
+    _putchar("p")
+    _putchar("u")
+    _putchar("t")
+    _putchar("c")
+    _putchar("h")
+    _putchar("a")
+    _putchar("r")
+	return (0);
+}
