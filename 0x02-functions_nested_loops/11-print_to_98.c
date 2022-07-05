886ef1522616ddf25331e57ff6101311a976fda3
Nebyu <nebyouabayneh@gmail.com>
2022-07-05 21:55:49 +0300

    It prints whole number starting from n to 98 and 98 to n in order

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_to_98 - Check description
+ * Description: It prints whole number starting from n to 98
+ * and 98 to n in order
+ * Return: Nothing.
+ */
+void print_to_98(int n)
+{
+	if (n <= 98)
+	{
+		for (; n <= 98; n++)
+		{
+			printf("%d", n);
+
+			if (n == 98)
+				continue;
+			printf(", ");
+		}
+		printf("\n");
+	}
+	else
+	{
+		for (; n >= 98; n--)
+		{
+			printf("%d", n);
+
+			if (n == 98)
+				continue;
+			printf(", ");
+		}
+		printf("\n");
+	}
+}
+
+
