cfca8b9df467ea14e4734bcc940ae0a50563ebf9
Nebyu <nebyouabayneh@gmail.com>
2022-07-11 16:14:30 +0300

    To find the length of a string

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _strlen - check the code for Holberton School students.
+ * @s: declaration of *s and paramters for the function _strlen
+ * Return: Always 0.
+ */
+int _strlen(char *s)
+{
+	int string;
+
+	for (string = 0; s[string] != 0; string++)
+	{
+	}
+	return (string);
+}
