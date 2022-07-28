bae0a856e226bbaf9a1c2ce72d0a791e13c570e7
Nebyu <nebyouabayneh@gmail.com>
2022-07-28 17:49:21 +0300

    Dinamic memory allocation

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -8,10 +8,10 @@
 
 unsigned int _strlen(char *s)
 {
-unsigned int size = 0;
-for (; s[size] != '\0'; size++)
-;
-return (size);
+	unsigned int size = 0;
+	for (; s[size] != '\0'; size++)
+		;
+	return (size);
 }
 
 /**
@@ -24,29 +24,29 @@ return (size);
 
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-unsigned int i, j;
-char *m;
+	unsigned int i, j;
+	char *m;
 
-if (s1 == NULL)
-	s1 = "";
-if (s2 == NULL)
-	s2 = "";
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
 
-if (n < _strlen(s2))
-	m = malloc(_strlen(s1) + n * sizeof(char) + 1);
-else
-	m = malloc(_strlen(s1) + _strlen(s2) + 1);
+	if (n < _strlen(s2))
+		m = malloc(_strlen(s1) + n * sizeof(char) + 1);
+	else
+		m = malloc(_strlen(s1) + _strlen(s2) + 1);
 
-if (m == 0)
-	return (NULL);
+	if (m == 0)
+		return (NULL);
 
-for (i = 0; s1[i] != '\0'; i++)
-	m[i] = s1[i];
+	for (i = 0; s1[i] != '\0'; i++)
+		m[i] = s1[i];
 
-for (j = 0; s2[j] != '\0' && j < n; i++, j++)
-	m[i] = s2[j];
+	for (j = 0; s2[j] != '\0' && j < n; i++, j++)
+		m[i] = s2[j];
 
-m[i] = '\0';
+	m[i] = '\0';
 
-return (m);
+	return (m);
 }
